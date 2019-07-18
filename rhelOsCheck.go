a76bfa3f71271cf57192509f1380e842d3d4ca01
Nasr Siddiqui <nasr.siddiqui1@gmail.com>
2019-07-18 16:25:26 -0700

    Closing the file handle before the function exits

--- rhelOsCheck.go
+++ rhelOsCheck.go
@@ -9,19 +9,23 @@ import (
 func OsCheck() (OsVersion []byte, err error) {
         f, err := os.Open("/etc/redhat-release")
         if err != nil {
+                f.Close()
                 return nil, err
         }
 
         if _, err := f.Seek(40, 0); err != nil {
+                f.Close()
                 return nil, err
         }
 
         b1 := make([]byte, 1)
         os, err := f.Read(b1)
         if err != nil {
+                f.Close()
                 return nil, err
         }
 
+        f.Close()
         return b1[:os], err
 }
 
